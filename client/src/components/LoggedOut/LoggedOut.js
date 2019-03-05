2eae03957891d4881cdb76f25e0984ecbd11ea93
Meng Vang <meng.vang92@yahoo.com>
2019-03-04 18:42:08 -0600

    modified NoMatch page

--- client/src/components/LoggedOut/LoggedOut.js
+++ client/src/components/LoggedOut/LoggedOut.js
@@ -8,9 +8,11 @@ const LoggedOut = () =>
     <div className="row mt-4">
       <div className="col-md-6">
         <label><h2>Easily record service records to multiple vehicles through CarLog.</h2></label>
+      </div>
+      <div className="col-md-6">
         <div className="row mt-3">
           <div className="col-md-2">
-            <img class="notLoggedInImages" src={createAccountLogo} alt="Create Account"></img>
+            <img className="notLoggedInImages" src={createAccountLogo} alt="Create Account"></img>
           </div>
           <div className="col-md-10">
             <label><h6><strong>Create an account</strong> to start adding service records.</h6></label>
@@ -18,7 +20,7 @@ const LoggedOut = () =>
         </div>
         <div className="row mt-3">
           <div className="col-md-2">
-            <img class="notLoggedInImages" src={recordLogsLogo} alt="Record Services"></img>
+            <img className="notLoggedInImages" src={recordLogsLogo} alt="Record Services"></img>
           </div>
           <div className="col-md-10">
             <label><h6><strong>Record service information</strong> for your vehicles.</h6></label>
@@ -26,15 +28,12 @@ const LoggedOut = () =>
         </div>
         <div className="row mt-3">
           <div className="col-md-2">
-            <img class="notLoggedInImages" src={manageDataLogo} alt="Manage Data"></img>
+            <img className="notLoggedInImages" src={manageDataLogo} alt="Manage Data"></img>
           </div>
           <div className="col-md-10">
             <label><h6><strong>Manage data</strong> through an easy to navigate user interface.</h6></label>
           </div>
         </div>
-      </div>
-      <div className="col-md-6">
-        null
       </div>
     </div>
   </React.Fragment>
--- client/src/pages/NoMatch/NoMatch.js
+++ client/src/pages/NoMatch/NoMatch.js
@@ -1,10 +1,10 @@
 import React from "react";
 
 const NoMatch = () => (
-  <div className="text-center text-light">
-    No Match
+  <div className="text-center text-danger mt-3">
+    <label><h3>Invalid route or no permission to view content.</h3></label>
     <br />
-    <a href="/"><button>← Back Home</button></a>
+    <a  href="/"><button className="backHomeBtn">← Back Home</button></a>
   </div>
 );
 
