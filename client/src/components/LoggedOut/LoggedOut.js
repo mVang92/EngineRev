e97ba0f8fbf24701dab77c907f107e005b90bfae
Meng Vang <meng.vang92@yahoo.com>
2019-03-04 19:06:01 -0600

    added gifs to NoMatch and NotLoggedIn pages

--- client/src/components/LoggedOut/LoggedOut.js
+++ client/src/components/LoggedOut/LoggedOut.js
@@ -2,12 +2,18 @@ import React from "react";
 import createAccountLogo from "../../images/createAccount.png"
 import recordLogsLogo from "../../images/recordLogs.png"
 import manageDataLogo from "../../images/manageData.png"
+import notLoggedInCarGif from "../../images/notLoggedInCarGif.gif"
 
 const LoggedOut = () =>
   <React.Fragment>
-    <div className="row mt-4">
+    <div className="row mt-5">
       <div className="col-md-6">
-        <label><h2>Easily record service records to multiple vehicles through CarLog.</h2></label>
+        <label><h2>Easily record service logs to your cars through CarLog.</h2></label>
+        <div className="row">
+          <div className="col-md-12">
+            <img id="notLoggedInCarGif" src={notLoggedInCarGif} alt="Create Account"></img>
+          </div>
+        </div>
       </div>
       <div className="col-md-6">
         <div className="row mt-3">
@@ -15,7 +21,7 @@ const LoggedOut = () =>
             <img className="notLoggedInImages" src={createAccountLogo} alt="Create Account"></img>
           </div>
           <div className="col-md-10">
-            <label><h6><strong>Create an account</strong> to start adding service records.</h6></label>
+            <label><h6><strong>Create an account</strong> to start adding service logs.</h6></label>
           </div>
         </div>
         <div className="row mt-3">
@@ -23,10 +29,10 @@ const LoggedOut = () =>
             <img className="notLoggedInImages" src={recordLogsLogo} alt="Record Services"></img>
           </div>
           <div className="col-md-10">
-            <label><h6><strong>Record service information</strong> for your vehicles.</h6></label>
+            <label><h6><strong>Record service logs</strong> for your vehicles.</h6></label>
           </div>
         </div>
-        <div className="row mt-3">
+        <div className="row mt-4">
           <div className="col-md-2">
             <img className="notLoggedInImages" src={manageDataLogo} alt="Manage Data"></img>
           </div>
--- client/src/pages/NoMatch/NoMatch.js
+++ client/src/pages/NoMatch/NoMatch.js
@@ -1,10 +1,19 @@
 import React from "react";
+import noMatchGif from "../../images/noMatch.gif";
 
 const NoMatch = () => (
   <div className="text-center text-danger mt-3">
     <label><h3>Invalid route or no permission to view content.</h3></label>
     <br />
-    <a  href="/"><button className="backHomeBtn">← Back Home</button></a>
+    <a href="/"><button className="backHomeBtn">← Back Home</button></a>
+    <br />
+    <div className="row">
+      <div className="col-md-4"></div>
+      <div className="col-md-4">
+        <img className="notLoggedInImages" src={noMatchGif} alt="No Match"></img>
+      </div>
+      <div className="col-md-4"></div>
+    </div>
   </div>
 );
 
