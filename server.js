2ff30d454ff81c00f42bfc5866764d44c5c91dcd
mVang92 <meng.vang92@yahoo.com>
2019-10-27 19:38:50 -0500

    modify server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 3001;
 // Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static("public"));
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -16,7 +17,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/carLog");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/carSpace");
 
 // Start the API server
 app.listen(PORT, function() {
