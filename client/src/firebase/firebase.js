bcfc368e02afd3d121c8c6d74010ab9c47479260
mVang92 <meng.vang92@yahoo.com>
2019-10-27 19:26:18 -0500

    modify firebase.js file

--- client/src/firebase/firebase.js
+++ client/src/firebase/firebase.js
@@ -1,7 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 
-const prodConfig = {
+const config = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
@@ -10,20 +10,6 @@ const prodConfig = {
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_ID
 };
-
-const devConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_ID
-};
-
-const config = process.env.NODE_ENV === "production"
-  ? prodConfig
-  : devConfig;
 
 if (!firebase.apps.length) {
   firebase.initializeApp(config);
