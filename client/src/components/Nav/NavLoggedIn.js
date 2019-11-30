7e0ded562272bcf647731f0022bb5fe2ea05659e
mVang92 <meng.vang92@yahoo.com>
2019-11-30 10:14:30 -0600

    add ids to input boxes and buttons for testing

--- client/src/components/Nav/NavLoggedIn.js
+++ client/src/components/Nav/NavLoggedIn.js
@@ -3,7 +3,7 @@ import React from "react";
 export const NavLoggedIn = props => {
     return (
         <React.Fragment>
-            <div className="nav-item text-light nav-link underline" onClick={(event) => props.showSignOutModal(event)}>
+            <div id="signOutNavButton" className="nav-item text-light nav-link underline" onClick={(event) => props.showSignOutModal(event)}>
                 Sign Out
             </div>
         </React.Fragment>
