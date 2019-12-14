79997039c056360d5515f89e5c7b0e736d706548
mVang92 <meng.vang92@yahoo.com>
2019-12-13 19:48:48 -0600

    word wrapped the userEmail

--- client/src/components/Nav/NavLoggedIn.js
+++ client/src/components/Nav/NavLoggedIn.js
@@ -4,16 +4,16 @@ import carSpaceLogo from "../../images/carSpaceLogo.png";
 export const NavLoggedIn = props => {
     return (
         <React.Fragment>
-            <nav class="navbar navbar-expand-lg navbar-light">
+            <nav className="navbar navbar-expand-lg navbar-light">
                 <a className="navbar-brand underline" href="/">
                     <img id="applicationLogo" src={carSpaceLogo} alt="applicationLogo"></img>
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item active">
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item active">
                             <a id="signOutNavButton" className="nav-item nav-link underline" onClick={(event) => props.showSignOutModal(event)}>
                                 Sign Out
                          </a>
