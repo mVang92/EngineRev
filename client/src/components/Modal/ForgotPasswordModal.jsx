71323ba24251c7883568bc4e1ed17c302918a827
Meng Vang <meng.vang92@yahoo.com>
2019-12-28 14:39:41 -0600

    add feature to send password reset confirmation

---
+++ client/src/components/Modal/ForgotPasswordModal.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Modal from "react-modal";
+
+const ForgotPasswordModal = props => {
+    return (
+        <Modal
+            isOpen={props.showForgotPasswordModal}
+            className="Modal__Bootstrap modal-dialog"
+            shouldCloseOnOverlayClick={true}
+            closeTimeoutMS={0}
+        >
+            <div className="accountModal modal-content">
+                <div className="modal-body modalShadow">
+                    <form className="modalBody" onSubmit={props.handlePasswordReset}>
+                        <div className="modal-header text-center">
+                            <label><strong>Enter your email address to send a password reset confirmation.</strong></label>
+                        </div>
+                        <hr />
+                        <div className="modal-body">
+                            <input id="emailInputForPasswordReset" type="text" value={props.email} name="email" onChange={props.handleChange} placeholder="Email"></input>
+                            <br /><br />
+                        </div>
+                        <div className="modal-footer">
+                            <button id="closeForgotPasswordModal" type="button" className="btn btn-light" onClick={props.hideForgotPasswordModal} data-dismiss="modal">Close</button>
+                            <button id="forgotPasswordSubmitButton" className="btn btn-light" type="submit">Send Confirmation</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </Modal>
+    );
+};
+
+export default ForgotPasswordModal;
