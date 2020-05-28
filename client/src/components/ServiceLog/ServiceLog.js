e8c637073956b49566c8fcacc0f16b1b982f3dda
mVang92 <meng.vang92@yahoo.com>
2020-05-27 22:52:21 -0500

    refactor servicelog component

--- client/src/components/ServiceLog/ServiceLog.js
+++ client/src/components/ServiceLog/ServiceLog.js
@@ -1,31 +1,30 @@
 import React from "react";
 
 const ServiceLog = props => {
-  const dateToFormat = props.date;
-  const mileage = props.mileage;
-  const service = props.service;
-  const comment = props.comment;
-  const serviceLogId = props._id;
+  const {
+    _id,
+    date,
+    mileage,
+    service,
+    comment,
+    showEditOneServiceLogModal,
+    currentTheme
+  } = props;
 
-  const serviceLogDate = new Date(dateToFormat);
+  const serviceLogDate = new Date(date);
   const currentDate = new Date();
-
   serviceLogDate.setHours(0, 0, 0, 0) > new Date().setHours(0, 0, 0, 0);
   currentDate.setHours(0, 0, 0, 0) > new Date().setHours(0, 0, 0, 0);
-
   serviceLogDate.setDate(serviceLogDate.getDate() + 1);
   currentDate.setDate(currentDate.getDate());
-
   const serviceLogDateFormatted = serviceLogDate.toLocaleDateString("en-US");
   const currentDateFormatted = currentDate.toLocaleDateString("en-US");
   let serviceLogFullDetails;
-
   if (currentDateFormatted === serviceLogDateFormatted) {
     serviceLogFullDetails = {
       background: "rgb(200, 255, 200)"
     };
   };
-
   if (currentDate.getTime() < serviceLogDate.getTime()) {
     serviceLogFullDetails = {
       background: "rgb(255, 200, 200)"
@@ -33,9 +32,9 @@ const ServiceLog = props => {
   };
 
   return (
-    <React.Fragment key={serviceLogId}>
+    <React.Fragment key={_id}>
       <div className="serviceLog">
-        <div className={`row serviceLogFullDetails ${props.currentTheme.serviceLogs}`} style={serviceLogFullDetails}>
+        <div className={`row serviceLogFullDetails ${currentTheme.serviceLogs}`} style={serviceLogFullDetails}>
           <div className="col-md-2 logDetailsMobileDisplay"><span className="showUnderMobileDisplay"><strong>Date: </strong></span>{serviceLogDateFormatted}</div>
           <div className="col-md-2 logDetailsMobileDisplay"><span className="showUnderMobileDisplay"><strong>Mileage: </strong></span>{mileage} miles</div>
           <div className="col-md-3 logDetailsMobileDisplay"><span className="showUnderMobileDisplay"><strong>Service: </strong></span>{service}</div>
@@ -58,7 +57,7 @@ const ServiceLog = props => {
                 <button
                   className="editActionButton"
                   title="Edit Service Log"
-                  onClick={() => props.showEditOneServiceLogModal(serviceLogId, serviceLogDateFormatted, mileage, service, comment)}>
+                  onClick={() => showEditOneServiceLogModal(_id, serviceLogDateFormatted, mileage, service, comment)}>
                   Edit
                 </button>
               </div>
