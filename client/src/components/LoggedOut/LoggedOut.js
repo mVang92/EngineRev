2550ee33f719a3495f3ed8fe3778d812fed4386a
mVang92 <meng.vang92@yahoo.com>
2020-10-18 22:42:23 -0500

    wip logged out home screen

--- client/src/components/LoggedOut/LoggedOut.js
+++ client/src/components/LoggedOut/LoggedOut.js
@@ -3,51 +3,44 @@ import Footer from "../Footer";
 import createAccountLogo from "../../images/createAccount.png"
 import recordLogsLogo from "../../images/recordLogs.png"
 import collaborate from "../../images/collaborate.png"
-import notLoggedIn from "../../images/notLoggedIn.png"
+import engineRevLogo from "../../images/engineRevLogo.png"
 
 const LoggedOut = () => {
   return (
     <React.Fragment>
       <div className="row section noMarginDesktopDisplay">
-        <div id="heading" className="col-md-12 text-center">
-          <br /><br />
-          <div><label><h2>Welcome!</h2></label></div>
-          <hr />
-          <div><img id="notLoggedInMainImage" src={notLoggedIn} alt="Create Account"></img></div>
-        </div>
-      </div>
-      <div className="row section noMarginDesktopDisplay">
-        <div className="col-md-12">
-          <div className="mt-3">
-            <div className="row">
-              <div className="col-md-4">
-                <div className="row">
-                  <div className="col-md-3">
-                    <img className="notLoggedInImages" src={createAccountLogo} alt="Create Account"></img>
-                  </div>
-                  <div className="col-md-9">
-                    <label><h6><strong>Create an account</strong> to track vehicle service logs.</h6></label>
-                  </div>
+        <div id="homePageContent" className="col-md-12">
+          <div className="row">
+            <div className="col-md-3">
+              <img id="frontLogo" src={engineRevLogo} alt="EngineRev"></img>
+            </div>
+            <div className="col-md-3 smallMarginTopMobileDisplay">
+              <div className="row">
+                <div className="col-md-3">
+                  <img className="notLoggedInImages" src={createAccountLogo} alt="Create Account"></img>
+                </div>
+                <div className="col-md-9">
+                  <label><h6><strong>Create an account</strong> to track vehicle service logs.</h6></label>
                 </div>
               </div>
-              <div className="col-md-4">
-                <div className="row">
-                  <div className="col-md-3">
-                    <img className="notLoggedInImages" src={recordLogsLogo} alt="Record Services"></img>
-                  </div>
-                  <div className="col-md-9">
-                    <label><h6><strong>Log service history</strong> for multiple vehicles.</h6></label>
-                  </div>
+            </div>
+            <div className="col-md-3 smallMarginTopMobileDisplay">
+              <div className="row">
+                <div className="col-md-3">
+                  <img className="notLoggedInImages" src={recordLogsLogo} alt="Record Services"></img>
+                </div>
+                <div className="col-md-9">
+                  <label><h6><strong>Log service history</strong> for multiple vehicles.</h6></label>
                 </div>
               </div>
-              <div className="col-md-4">
-                <div className="row">
-                  <div className="col-md-3">
-                    <img className="notLoggedInImages" src={collaborate} alt="Collaborate"></img>
-                  </div>
-                  <div className="col-md-9">
-                    <label><h6><strong>Collaborate with others</strong> through the forum.</h6></label>
-                  </div>
+            </div>
+            <div className="col-md-3 smallMarginTopMobileDisplay">
+              <div className="row">
+                <div className="col-md-3">
+                  <img className="notLoggedInImages" src={collaborate} alt="Collaborate"></img>
+                </div>
+                <div className="col-md-9">
+                  <label><h6><strong>Collaborate with others</strong> through the forum.</h6></label>
                 </div>
               </div>
             </div>
