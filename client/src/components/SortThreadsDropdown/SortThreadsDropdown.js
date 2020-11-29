5682a157a465576940533c7740d2663552e3d9dc
mVang92 <meng.vang92@yahoo.com>
2020-11-28 23:33:35 -0600

    sort thread wip

---
+++ client/src/components/SortThreadsDropdown/SortThreadsDropdown.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { defaults } from "../../assets/Defaults";
+
+const SortThreadsDropdown = props => {
+
+  return (
+    <div id="category" className="text-center">
+      <span>Sort By: </span>
+      <select id={defaults.sortThreadsDropdown} name="sortThreads" defaultValue={props.threadCategory}>
+        <option value={defaults.oldestThreadsSort}>Oldest</option>
+        <option value={defaults.mostRecentThreadsSort}>Most Recent</option>
+        <option value={defaults.mostViewsThreadsSort}>Views</option>
+        <option value={defaults.mostCommentsThreadsSort}>Comments</option>
+        <option value={defaults.askCarQuestionsSort}>Ask Car Question</option>
+        <option value={defaults.tipsAndTricksThreadsSort}>Tips and Tricks</option>
+        <option value={defaults.shareStoryThreadsSort}>Share a Story</option>
+        <option value={defaults.otherCategoryThreadSort}>Other Category</option>
+      </select>
+    </div>
+  )
+};
+
+export default SortThreadsDropdown;
