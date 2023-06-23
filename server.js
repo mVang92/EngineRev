09424a248d31e930b28aec00e7e90800bdfb32f6
Meng Vang <meng.vang92@yahoo.com>
2023-06-23 09:35:01 -0500

    changed proxy port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const axios = require("axios")
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 // Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }));
